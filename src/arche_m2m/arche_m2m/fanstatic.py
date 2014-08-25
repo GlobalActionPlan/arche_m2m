f2cbcbc58d857c424700345e407b7db80b9068bb
robinharms <robin@betahaus.net>
2014-08-25 11:42:16 +0200

    Dev structure

---
+++ src/arche_m2m/arche_m2m/fanstatic.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+from fanstatic import Library
+from fanstatic import Resource
+from js.bootstrap import bootstrap
+from js.jquery import jquery
+
+
+lib_m2m = Library("arche_m2m", "static")
---
+++ src/arche_m2m/setup.py
@@ -0,0 +1,46 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
+
+requires = [
+    'Arche',
+    'pyramid',
+    'fanstatic',
+    'lingua',
+    'Babel',
+    ]
+
+setup(name='arche_m2m',
+      version='0.1dev',
+      description='Made to Measure',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Development Status :: 3 - Alpha",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='M2M development team and contributors',
+      author_email='robin@betahaus.net',
+      url='https://github.com/GlobalActionPlan/arche_m2m',
+      keywords='web pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite="arche_m2m",
+      entry_points="""\
+      [fanstatic.libraries]
+      arche_m2m = arche_m2m.fanstatic:lib_m2m
+      """,
+      message_extractors = { '.': [
+              ('**.py',   'lingua_python', None ),
+              ('**.pt',   'lingua_xml', None ),
+              ]},
+      )
