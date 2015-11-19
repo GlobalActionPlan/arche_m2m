c04ddb417f8ffb271dacaec137b01787b5c64590
robinharms <robin@betahaus.net>
2015-11-19 17:02:34 +0100

    Fixed export function

--- src/arche_m2m/arche_m2m/views/export.py
+++ src/arche_m2m/arche_m2m/views/export.py
@@ -5,20 +5,58 @@ from arche.views.base import BaseView
 from pyramid.httpexceptions import HTTPForbidden
 from pyramid.response import Response
 from pyramid.view import view_config
-from pyramid.view import view_defaults
 from six import StringIO
 
 from arche_m2m import _
 from arche_m2m.interfaces import IQuestionWidget
 from arche_m2m.interfaces import ISurvey
 from arche_m2m.interfaces import ISurveySection
+from arche_m2m.interfaces import IQuestion
 
 
-@view_defaults(context = ISurvey, permission = security.PERM_VIEW)
-class ExportView(BaseView):
+class BaseExportView(BaseView):
 
-    @view_config(name = 'export.csv')
-    def csv(self):
+    def get_sections(self):
+        for obj in self.context.values():
+            if ISurveySection.providedBy(obj):
+                yield obj
+
+    def get_question_type(self, question):
+        question_type = self.resolve_uid(question.question_type, perm = None)
+        if not question_type:
+            raise HTTPForbidden("%r has no question type set." % question)
+        return question_type
+
+    def get_widget(self, question_type):
+        if IQuestion.providedBy(question_type):
+            question_type = self.get_question_type(question_type)
+        question_widget = self.request.registry.queryAdapter(question_type,
+                                                             IQuestionWidget,
+                                                             name = getattr(question_type, 'input_widget', ''))
+        if not question_widget:
+            raise HTTPForbidden("%r has no question widget set." % question_type)
+        return question_widget
+
+    def get_choices(self, question):
+        assert IQuestion.providedBy(question)
+        question_type = self.get_question_type(question)
+        results = [x for x in question_type.get_choices(self.request.locale_name)]
+        results.extend([x for x in question.get_choices(self.request.locale_name)])
+        return results
+
+    def get_choice_sorted_questions(self, section):
+        results = {}
+        for question in section.get_questions(self.request.locale_name, resolve = True):
+            choices = tuple([x.cluster for x in self.get_choices(question)])
+            curr = results.setdefault(choices, [])
+            curr.append(question)
+        return results
+
+
+@view_config(name = 'export.csv', context = ISurvey, permission = security.PERM_VIEW)
+class CSVExport(BaseExportView):
+
+    def __call__(self):
         output = StringIO()
         lang_name = self.request.locale_name
         writer = csv.writer(output, delimiter=';', quotechar='"', quoting=csv.QUOTE_ALL, dialect = csv.excel)
@@ -30,89 +68,37 @@ class ExportView(BaseView):
             lrow.append(lang)
         writer.writerow(lrow)
         writer.writerow([])
-        for section in self.context.values():
-            if not ISurveySection.providedBy(section):
-                continue
+        for section in self.get_sections():
             writer.writerow([])
             writer.writerow([_('Section:'), section.title.encode('utf-8')])
             writer.writerow([_('Responses:'), len(section.responses)])
             writer.writerow([])
-
-            for question in section.get_questions(lang_name, resolve = True):
-                question_type = self.resolve_uid(question.question_type, perm = None)
-                if not question_type:
-                    raise HTTPForbidden("%r has no question type set." % question)
-                question_widget = self.request.registry.queryAdapter(question_type,
-                                                                     IQuestionWidget,
-                                                                     name = getattr(question_type, 'input_widget', ''))
-                if not question_widget:
-                    raise HTTPForbidden("%r has no question widget set." % question_type)
-                response = question_widget.responses(section, question)
-                writer.writerow([question.title.encode('utf-8')])
-                #Choice responses
-                if isinstance(response, dict):
-                    choices = dict([(choice.cluster, choice.title.encode('utf-8')) for choice in question_type.get_choices(lang_name)])
-                    choices.update([(choice.cluster, choice.title.encode('utf-8')) for choice in question.get_choices(lang_name)])
-                    results = dict([(x, 0) for x in choices])
-                    results.update(response)
-                    for (k, v) in results.items():
-                        writer.writerow([choices.get(k, k), v])
-                #Text, number etc
-                else:
-                    for x in response:
-                        writer.writerow([x])
-                writer.writerow([])
+            for (choices, questions) in self.get_choice_sorted_questions(section).items():
+                write_header = True
+                for question in questions:
+                    question_type = self.get_question_type(question)
+                    question_widget = self.get_widget(question_type)
+                    choice_objects = self.get_choices(question)
+                    if write_header:
+                        row = ['']
+                        for choice in choice_objects:
+                            row.append(choice.title.encode('utf-8'))
+                        writer.writerow(row)
+                        write_header = False
+                    response = question_widget.responses(section, question)
+                    if isinstance(response, dict):
+                        results = dict([(x, 0) for x in choices])
+                        results.update(response)
+                        out = [question.title.encode('utf-8')]
+                        for choice in choice_objects:
+                            out.append(results.get(choice.cluster))
+                        writer.writerow(out)
+                    else:
+                        writer.writerow([])
+                        writer.writerow([question.title.encode('utf-8')])
+                        for x in response:
+                            if x:
+                                writer.writerow([x])
         contents = output.getvalue()
         output.close()
         return Response(content_type = 'text/csv', body = contents)
-
-#FIXME: Rework text export
-#    @view_config(name = 'export.txt')
-#    def txt(self):
-#        """
-#            I didn't modify the export function I created my own function(copy of csv()). I just use the print to see the result.
-#
-#        """
-#        output = StringIO()
-#        lang_name = self.request.locale_name
-#        #print(self.context.title.encode('utf-8'))
-#        #print('Export using language:', lang_name)
-#
-#        for section in self.context.values():
-#            if not ISurveySection.providedBy(section):
-#                continue
-#            print('Section:' +  section.title.encode('utf-8'))
-#            print('Responses:' + str(len(section.responses)))
-#
-#            section.get_question_id(lang_name)
-#
-#            for question in section.get_questions(lang_name, resolve = True):
-#                question_type = self.resolve_uid(question.question_type, perm = None)
-#
-#                if not question_type:
-#                    raise HTTPForbidden("%r has no question type set." % question)
-#                question_widget = self.request.registry.queryAdapter(question_type,
-#                                                                     IQuestionWidget,
-#                                                                     name = getattr(question_type, 'input_widget', ''))
-#                if not question_widget:
-#                    raise HTTPForbidden("%r has no question widget set." % question_type)
-#                response = question_widget.responses(section, question)
-#                print(question.title.encode('utf-8'))
-#                #Choice responses
-#                if isinstance(response, dict):
-#                    choices = dict([(choice.cluster, choice.title.encode('utf-8')) for choice in question_type.get_choices(lang_name)])
-#                    choices.update([(choice.cluster, choice.title.encode('utf-8')) for choice in question.get_choices(lang_name)])
-#                    results = dict([(x, 0) for x in choices])
-#                    results.update(response)
-#
-#                    for (k, v) in results.items():
-#                        print(choices.get(k,k),v)
-#                #Text, number etc
-#                else:
-#                    for x in response:
-#                        print(x)
-#
-#            # not a good idea but I could distincly see
-#            #section.responses=OOBTree()
-#        contents = ''
-#        return Response(content_type = 'text/txt', body = contents)
