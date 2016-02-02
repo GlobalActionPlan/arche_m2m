20d923eab79be77c9d5de18db73cbc372e301b3b
Robin Harms Oredsson <robin@betahaus.net>
2016-02-02 15:07:55 +0100

    Files had moved

--- update_translations.sh
+++ update_translations.sh
@@ -2,9 +2,8 @@
  #You need lingua and gettext installed to run this
  
  echo "Updating arche_m2m.pot"
- pot-create -d arche_m2m -o src/arche_m2m/arche_m2m/locale/arche_m2m.pot src/arche_m2m/arche_m2m
- #pot-create -d arche_m2m_user -o src/arche_m2m/arche_m2m/locale/arche_m2m_user.pot src/arche_m2m/arche_m2m
- 
+ pot-create -d arche_m2m -o arche_m2m/locale/arche_m2m.pot arche_m2m
+
  echo "Merging Swedish localisation"
- msgmerge --update  src/arche_m2m/arche_m2m/locale/sv/LC_MESSAGES/arche_m2m.po  src/arche_m2m/arche_m2m/locale/arche_m2m.pot
+ msgmerge --update  arche_m2m/locale/sv/LC_MESSAGES/arche_m2m.po  arche_m2m/locale/arche_m2m.pot
  echo "Updated locale files"
